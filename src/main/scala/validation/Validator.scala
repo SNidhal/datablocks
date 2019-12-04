7451bceac1eed63a14e491751c7432360689da03
nidhal.saadaoui1@esprit.tn <Azerty.199346>
2019-12-04 09:46:17 +0100

    drop null

--- src/main/scala/validation/Validator.scala
+++ src/main/scala/validation/Validator.scala
@@ -1,5 +1,30 @@
 package validation
 
+import org.apache.spark.sql.DataFrame
+
 object Validator {
+  def dropNull(df: DataFrame): DataFrame = {
+    df.na.drop()
+  }
+
+  def dropNull(df: DataFrame, minNonNulls: Int): DataFrame = {
+    df.na.drop()
+  }
+
+  def dropNull(df: DataFrame, minNonNulls: Int, cols: Seq[String]): DataFrame = {
+    df.na.drop(minNonNulls, cols)
+  }
+
+  def dropNull(df: DataFrame, cols: Seq[String]): DataFrame = {
+    df.na.drop(cols)
+  }
+
+  def dropNull(df: DataFrame, how: String): DataFrame = {
+    df.na.drop(how)
+  }
+
+  def dropNull(df: DataFrame, how: String, cols: Seq[String]): DataFrame = {
+    df.na.drop(how, cols)
+  }
 
 }
