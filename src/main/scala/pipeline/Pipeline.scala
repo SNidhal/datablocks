c298d1a8820bd25878d90082c2c2d6811723029b
SNIDHAL <nidhal.saadaoui1@esprit.tn>
2020-01-15 15:33:21 +0100

    query handling case

--- src/main/scala/pipeline/Pipeline.scala
+++ src/main/scala/pipeline/Pipeline.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.types.{BooleanType, DataType, DoubleType, IntegerType, StringType, StructField, StructType}
 import org.yaml.snakeyaml.Yaml
 import sources.Reader
-import transformations.ColumnAppender
+import transformations.{ColumnAppender, QueryHandler}
 
 import scala.collection.JavaConverters._
 
@@ -13,7 +13,7 @@ import scala.collection.JavaConverters._
 object Pipeline {
 
 
-  def buildEtl(config : String)(implicit sparkSession: SparkSession): Etl = {
+  def buildEtl(config: String)(implicit sparkSession: SparkSession): Etl = {
     val configPath = scala.io.Source.fromFile(config).mkString
     val yml = new Yaml()
     val configuration = yml.load(configPath).asInstanceOf[java.util.Map[String, Object]]
@@ -33,23 +33,23 @@ object Pipeline {
     val format = configuration.get("writer").asInstanceOf[java.util.Map[String, String]].get("format")
     val compression = configuration.get("writer").asInstanceOf[java.util.Map[String, String]].get("compression")
     val writerOptions = configuration.get("writer").asInstanceOf[java.util.Map[String, Object]]
-      .getOrDefault("options",new java.util.HashMap[String,String]())
+      .getOrDefault("options", new java.util.HashMap[String, String]())
       .asInstanceOf[java.util.Map[String, String]].asScala
-    println(writerPath)
-    println( mode)
-    println(partitionedBy)
-    println(compression)
-    println(format)
-    println(writerOptions.toMap)
     val writer = Writer(writerPath, mode, partitionedBy, compression, format, writerOptions.toMap)
 
     val transformer = configuration.get("transformer").asInstanceOf[java.util.ArrayList[java.util.Map[String, String]]]
       .toArray().toList
       .map(x => x.asInstanceOf[java.util.Map[String, String]])
       .map(x => {
-        x.get("rule") match {
-          case "tagWithName" => {
-            val a : DataFrame => DataFrame = ColumnAppender.tagWithFileName(x.get("columnName"))
+        x.get("type") match {
+          case "ColumnAppender" => x.get("rule") match {
+            case "tagWithName" => {
+              val a: DataFrame => DataFrame = ColumnAppender.tagWithFileName(x.get("columnName"))
+              a
+            }
+          }
+          case "QueryHandler" => {
+            val a: DataFrame => DataFrame = QueryHandler.execute(x.get("query"))
             a
           }
         }
