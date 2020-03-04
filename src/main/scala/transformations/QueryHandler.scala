978c77f05b8a6e52742d4f63b60da474ff61c3c2
SNIDHAL <nidhal.saadaoui1@esprit.tn>
2020-03-04 10:01:08 +0100

    query handling functions

--- src/main/scala/transformations/QueryHandler.scala
+++ src/main/scala/transformations/QueryHandler.scala
@@ -4,7 +4,28 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object QueryHandler {
   def execute(query: String)(df: DataFrame)(implicit spark: SparkSession): DataFrame = {
-    df.createOrReplaceTempView("TMP")
+    val lowCaseQuery = query.toLowerCase
+    val fromIndex = lowCaseQuery.indexOf("from")
+    val tableName = query.substring(fromIndex + 4).trim.split(" ").head
+    df.createOrReplaceTempView(tableName)
     spark.sql(query)
   }
+
+  def executeScript(query: String)(implicit spark: SparkSession): DataFrame = {
+    val parsedQuery = query.split(" ").filter(x => x.contains(".json") ^ x.contains(".csv")).map(x => {
+      val df = spark.read.format("csv")
+        .option("header", "true")
+        .option("sep", ";")
+        .option("inferSchema", "true")
+        .load(x)
+      val implicitTableName = "TBL" + x.hashCode.abs
+      df.createOrReplaceTempView(implicitTableName)
+      x -> implicitTableName
+    }).toMap.foldLeft(query) {
+      (s, r) => s.replaceAll(r._1, r._2)
+    }
+    spark.sql(parsedQuery)
+
+  }
+
 }
