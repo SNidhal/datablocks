df5056f43ba59880d57e36627a1f4ab42cac041d
SNIDHAL <nidhal.saadaoui1@esprit.tn>
2020-03-04 09:58:21 +0100

    override toString on writer

--- src/main/scala/destinations/Writer.scala
+++ src/main/scala/destinations/Writer.scala
@@ -3,14 +3,26 @@ package destinations
 import org.apache.spark.sql.DataFrame
 
 case class Writer(path: String, mode: String, partitionedBy: String, compression: String
-                  , format: String, options: Map[String, String]) {
+                  , format: String, options: Map[String, String],tableName:String) {
+
+  override def toString: String = path+"-"+mode+"-"+partitionedBy+"-"+compression+"-"+format+"-"+options+"-"+tableName
+
   def write(dataFrame: DataFrame) {
-    dataFrame
-      .write
-      .format(format)
-      .partitionBy(partitionedBy)
-      .options(options)
-      .mode(mode)
-      .save(path)
+    format match {
+      case "table" => dataFrame
+        .write
+        .partitionBy(partitionedBy)
+        .options(options)
+        .mode(mode)
+        .saveAsTable(tableName)
+      case _ => dataFrame
+        .write
+        .format(format)
+        .partitionBy(partitionedBy)
+        .options(options)
+        .mode(mode)
+        .save(path)
+    }
+
   }
 }
